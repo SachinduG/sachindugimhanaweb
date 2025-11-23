154c3874f36ce4d909e02b6dd3d59122d6c350cd
Sachindu Gimhana <54847380+SachinduG@users.noreply.github.com>
2025-11-23 10:00:09 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,33 @@
-/**
-* Template Name: iPortfolio - v3.0.0
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+/* Standard Scrollspy Logic */
+const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+    return [...document.querySelectorAll(el)]
+  } else {
+    return document.querySelector(el)
+  }
+}
+
+const onscroll = (el, listener) => {
+  el.addEventListener('scroll', listener)
+}
+
+let navbarlinks = select('#navbar .scrollto', true)
+const navbarlinksActive = () => {
+  let position = window.scrollY + 200
+  navbarlinks.forEach(navbarlink => {
+    if (!navbarlink.hash) return
+    let section = select(navbarlink.hash)
+    if (!section) return
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('active')
+    } else {
+      navbarlink.classList.remove('active')
+    }
+  })
+}
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
 (function() {
   "use strict";
 
@@ -252,5 +276,6 @@
       mirror: false
     })
   });
+
 
 })()
